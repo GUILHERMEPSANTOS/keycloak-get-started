442d013396e636c32048945dd176eafc623a95b8
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-09-11 21:04:10 -0300

    feat: add web api

---
+++ src/Services/WebApi/Configuration/JwtConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace WebApi.Configuration
+{
+    public static class JwtConfig
+    {
+        public static IServiceCollection AddAuthConfiguration(this IServiceCollection services, bool IsDevelopment, string publicKeyJWT)
+        {
+            var authenticationBuilder = services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
+
+
+
+
+            return services;
+        }
+
+        public static WebApplication UseAuthConfiguration(this WebApplication app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
---
+++ src/Services/WebApi/Program.cs
@@ -0,0 +1,28 @@
+using WebApi.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthConfiguration(IsDevelopment: true, publicKeyJWT: "");
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthConfiguration();
+
+app.MapControllers();
+
+app.Run();
+
